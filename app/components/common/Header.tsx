e35625188713d5f9e04a71adbbf344649eb2f3ac
JunggyeMessi <herrymh@naver.com>
2024-11-04 23:09:51 +0900

    ver 1.0.6

--- app/components/common/Header.tsx
+++ app/components/common/Header.tsx
@@ -7,10 +7,12 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <Image src={logo} alt="logo" className={styles.logo} />
-      <div className={styles.phone}>
-        <Image src={ico_phone} alt="icon-phone" className={styles.icon} />
-        <div>010-8699-7275</div>
-      </div>
+      <a href="tel:01086997275">
+        <div className={styles.phone}>
+          <Image src={ico_phone} alt="icon-phone" className={styles.icon} />
+          <div>010-8699-7275</div>
+        </div>
+      </a>
     </header>
   );
 };
